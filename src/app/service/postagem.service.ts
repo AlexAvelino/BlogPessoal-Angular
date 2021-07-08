0cc4be35923fa58f33c52396d851e46ceabfdd43
Alex <alourenco08@gmail.com>
2021-07-07 23:35:41 -0300

    Mudanças no Blog Pessoal

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -19,8 +19,20 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://blogglex.herokuapp.com/postagens',this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://blogglex.herokuapp.com/postagens/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://blogglex.herokuapp.com/postagens', postagem, this.token)
   }
+
+putPostagem(postagem: Postagem): Observable<Postagem>{
+  return this.http.put<Postagem>('https://blogglex.herokuapp.com/postagens', postagem, this.token)
+}
+
+deletePostagem(id: number){
+  return this.http.delete(`https://blogglex.herokuapp.com/postagens/${id}`, this.token)
+}
 
 }
